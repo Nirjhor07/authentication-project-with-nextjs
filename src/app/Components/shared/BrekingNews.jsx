4df801c868bc9298ed85d79387163030092cac0f
Ashraf Hossain Akash <nirjhorakash07@gmail.com>
2026-04-28 13:31:02 +0600

    dynamically marquee data shown

--- src/app/Components/shared/BrekingNews.jsx
+++ src/app/Components/shared/BrekingNews.jsx
@@ -1,17 +1,18 @@
+import { newsByCategories } from "@/lib/fetchdata";
 import Marquee from "react-fast-marquee";
 
-const BrekingNews = () => {
+const BrekingNews = async () => {
+  const response = await newsByCategories("07");
+  const brekingNews = response.data || [];
+  console.log(brekingNews);
+
+  // Combine all details with a separator
+  const allDetails = brekingNews.map((news) => news.details).join(" | ");
+
   return (
     <div className="container mx-auto flex gap-4">
       <button className="btn btn-error">Latest News</button>
-      <Marquee pauseOnHover={true}>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum rem
-          optio, natus quam culpa numquam minima itaque harum atque nostrum
-          veniam? Eligendi culpa rerum, sunt maxime numquam porro voluptas
-          veritatis.
-        </p>
-      </Marquee>
+      <Marquee pauseOnHover={true}>{allDetails}</Marquee>
     </div>
   );
 };
